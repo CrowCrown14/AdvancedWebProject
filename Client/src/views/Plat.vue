febab76a865ade5fcbf94b4d10cf12df42c04538
Alex <alex93120@hotmail.fr>
2021-11-07 06:18:49 +0100

    Feat : Add a part of shopping cart

--- Client/src/views/Plat.vue
+++ Client/src/views/Plat.vue
@@ -5,9 +5,22 @@
                     <img :src="getSrc(plat.image)" v-bind:alt="pic">
                 </div>
                 <div class="text">
-                    <p> id : {{ plat.id }}</p>
-                    <p> {{ plat.category }} : {{ plat.description }} </p>
+                    <p> <strong>id :</strong> {{ plat.id }}</p>
+                    <p> <strong>{{ plat.category }}</strong> : {{ plat.description }} </p>
                     <p> Prix : {{plat.price / 100}}€ </p>
+                </div>
+                <div>
+                    <div class="check">
+                        <Button v-on:click="removeQte()">-</Button>
+                        <p>Quantity : {{ this.qte }}</p>
+                        <Button v-on:click="addQte()">+</Button>
+                    </div>
+                    <br/>
+                    <div>
+                        <p>Total price : {{ plat.price /100 * this.qte }}€</p>
+                        <button v-on:click="addPanier()">Add to shopping cart</button>
+                    </div>
+                    
                 </div>
                 
             </div>
@@ -27,12 +40,25 @@ export default {
         return {
             menu : [],
             menuLength : 0,
+            qte : 0,
         }
     },
     methods: {
         getSrc(name) {
             var images = "http://localhost:3000" + name
             return images    
+        },
+        addQte() {
+            this.qte++;
+        },
+        removeQte() {
+            if (this.qte > 0)
+            {
+                this.qte--;
+            }
+        },
+        addPanier() {
+            alert("Added to shopping cart");
         }
     },
 }
@@ -40,6 +66,7 @@ export default {
 
 <style scoped>
     .miniBlock {    
+        padding-top: 20px;
         display: flex;
         align-items: center;
         margin: 3vh;
@@ -53,6 +80,13 @@ export default {
     .text {
         text-align: left;
         margin-left: 5vh;
+    }
+    .check {
+        display: flex;
+        padding-left:90vh ;
+        width: 300px;
+        height: 50px;
+        justify-content: right;
     }
 
 </style>
